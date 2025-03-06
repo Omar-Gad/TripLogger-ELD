eaf17c14be3a834d4a243eb65d041dd3822f4db3
Omar Gad <omargad9000@gmail.com>
2025-03-06 08:39:44 +0200

    CHANGE: {{project_name}}

--- backend/project_name/settings/base.py
+++ backend/project_name/settings/base.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     "django_guid.middleware.guid_middleware",
 ]
 
-ROOT_URLCONF = "{{project_name}}.urls"
+ROOT_URLCONF = "TripELD.urls"
 
 TEMPLATES = [
     {
@@ -87,7 +87,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "{{project_name}}.wsgi.application"
+WSGI_APPLICATION = "TripELD.wsgi.application"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
